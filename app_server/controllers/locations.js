7a6c9e2c1aecc872ada39d32c6543bbe2df1027e
akrishan <krishan.ash@gmail.com>
2016-02-22 21:18:36 +0000

    moved alllocations view variable data to controller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,8 +1,19 @@
 
 //All Locations list
 module.exports.listLocations = function(req, res) {
-    res.render('alllocations', { title: 'Express '});
+    var locationList = {
+        name: 'StarDucks',
+        rating: '3',
+        distance: '100m',
+        address: '123 lol street london, Ms4 5FG',
+        facilities: ['Hot Drinks', 'Food', 'Premium Wifi']
+    };
+    var description = 'This is the place to chill and is the side panel so that website could be responsive'
+    res.render('alllocations', { title: 'Express ', 
+                                description: description , 
+                                locationList: locationList });
 };
+
 
 //Show Locations Details
 module.exports.showLocation = function(req, res) {
